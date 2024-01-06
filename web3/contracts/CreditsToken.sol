d72208dd0ce21b294d85ff96997e2e5269dc1534
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2024-01-06 17:17:49 -0500

    testing started

--- web3/contracts/CreditsToken.sol
+++ web3/contracts/CreditsToken.sol
@@ -4,39 +4,55 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Credits is ERC20 {
-    address public owner;
-
-    uint public tokenPrice;
+    address private _owner;
+    uint private _tokenPrice;
 
     event TokensRedeemed(address indexed redeemer, uint indexed amount);
 
     modifier onlyOwner {
-        require(msg.sender == owner, "CREDITS: only owner function");
+        require(msg.sender == _owner, "CREDITS: only owner function");
         _;
     }
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
-        owner = msg.sender;
+    constructor(string memory _name, string memory _symbol, uint tp) ERC20(_name, _symbol){
+        _owner = msg.sender;
+        _tokenPrice = tp;
     }
 
 
     function mintTokens(uint numOfTokens) public payable {
-        require(msg.value >= tokenPrice, "CREDITS: invalid ether amount sent");
+        require(msg.value >= _tokenPrice, "CREDITS: invalid ether amount sent");
 
         _mint(msg.sender, numOfTokens);
 
     }
 
     // owner mint to address function to use for individual airdrops
-    function mintToAddress(uint numOfTokens, address target) public payable onlyOwner{
-        _mint(target, numOfTokens);
+    function mintToAddress( address target, uint amount) public payable onlyOwner{
+        _mint(target, amount);
     }
 
     // when users make redeem request, api request initiates the burn call from the owner wallet
     // tokens burned to redeem
-    function redeemCredits(uint amount, address target) public onlyOwner(){
+    function redeemCredits( address target, uint amount) public onlyOwner(){
         _burn(target, amount);
         emit TokensRedeemed(target, amount);
     }
 
     // CREATE A HELPER FUNCTION TO UPDATE TOKEN PRICE WHENEVER MINTING FUNCTION IS CALLED
+
+
+
+
+    // getter functions
+
+    function owner() public view returns(address){
+        return _owner;
+    }
+    function tokenPrice() public view returns(uint){
+        return _tokenPrice;
+    }
+
+    function getContractBalance()public view onlyOwner returns(uint){
+        return address(this).balance;
+    }
 }
--- web3/test/Credits.test.js
+++ web3/test/Credits.test.js
@@ -5,17 +5,52 @@ describe("Credits Contract", () =>{
     let CreditContract, deployer, user1, user2
     beforeEach(async ()=>{
         [deployer, user1, user2] = await ethers.getSigners()
-        CreditContract = await ethers.deployContract("Credits", ["Credit Token", "CT"])
+        CreditContract = await ethers.deployContract("Credits", ["Credit Token", "CT", ethers.parseUnits("1")])
         await CreditContract.waitForDeployment()
 
-        console.log(`Contract Deployed to ${CreditContract.target}`)
+        // console.log(`Contract Deployed to ${CreditContract.target}`)
         
     })
     describe("Deployment", () =>{
         it("checks owner address is deployer", async () =>{
             expect(await CreditContract.owner()).to.equal(deployer.address)
         })
-        it("checks the ")
+        it("checks the token price is 1 ether", async () =>{
+            expect(await CreditContract.tokenPrice()).to.equal(ethers.parseUnits("1"))
+        })
 
+    })
+    describe("Token Minting: User", () =>{
+        describe("Success", () =>{
+            beforeEach(async () =>{ 
+                await CreditContract.connect(user1).mintTokens(10,{value: ethers.parseUnits("10")})
+                
+            })
+            it("checks user1 balance", async () =>{
+                expect(await CreditContract.balanceOf(user1.address)).to.equal(10)
+            })
+            it("checks the contract balance to equal 10 eth", async () =>{
+                // TO DO ------ CHECK FOR FAILURE WHEN CALLED
+                expect(await CreditContract.getContractBalance()).to.equal(ethers.parseUnits("10"))
+            })
+        })
+        describe("Failure", () =>{
+            it("expects fail for not enough ether sent", async () =>{
+                await expect(CreditContract.mintTokens(10)).to.be.reverted
+            })
+            it("expects call to fail for owner not calling it", async () =>{
+                await expect(CreditContract.connect(user1).getContractBalance()).to.be.reverted
+            })
+        })
+    })
+    describe("Minting Tokens - Admin",() =>{
+        describe("Success",() =>{
+            beforeEach(async () =>{
+                await CreditContract.connect(deployer).mintToAddress(user1.address, 10)
+            })
+            it("expects user 1 balance to equal 10 tokens to redeem", async () =>{
+                expect(await CreditContract.balanceOf(user1.address)).to.equal(10)
+            })
+        })
     })
 })
