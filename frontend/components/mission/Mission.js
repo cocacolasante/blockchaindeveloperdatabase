d5a8a243c6038498975a455254c654dc374e70be
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2024-01-17 18:13:12 -0500

    about us and homepage

---
+++ frontend/components/mission/Mission.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Mission = () => {
+  return (
+    <div>
+        <h1>Our Mission</h1>
+        <p>While there are a lot of CRM, Tools, and Databases to help developers store their code and information, but unfortunately in the Blockchain Space
+            there is not a good solution to store smart contract address, abi's and deployer addresses... Until now!
+
+
+            Our goal is to simplify storing smart contract information for the smart contracts you develop. As you scale, we scale with you.
+        </p>
+    </div>
+  )
+}
+
+export default Mission
--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../../../components/about/About'
+import Mission from '../../../components/mission/Mission'
 
 const page = () => {
   return (
     <div>
       <About />
+      <Mission />
     </div>
   )
 }
